2d9dd43d68537e6be4739d635330c8bad769bddf
Michael Liao <askxuefeng@gmail.com>
2023-11-08 21:34:48 +0800

    fix test.

--- test/test_helper.mjs
+++ test/test_helper.mjs
@@ -1,28 +1,18 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
 
-import { loadLayout, loadTextFile, loadBinaryFile, loadYaml, executeTemplate } from '../bin/helper.mjs';
-
-test(`test executeTemplate`, (t) => {
-    let obj = {
-        site: {
-            name: 'GitSite',
-            description: 'A website tool',
-            author: {
-                'full-name': 'Crypto Michael',
-                email: '***@example.com'
-            }
-        },
-        version: 1
-    };
-    let templ = 'Welcome to ${site.name} v${version} by ${site.author.fullName} at ${site.release.date}';
-    let actual = executeTemplate(templ, obj);
-    assert.equal(actual, 'Welcome to GitSite v1 by Crypto Michael at undefined');
-});
+import { generateBookIndex, loadTextFile, loadBinaryFile, loadYaml } from '../bin/helper.mjs';
 
 test(`load yaml`, async (t) => {
     let obj = await loadYaml('.', 'test/resources/sample.yml');
     assert.equal(obj.site.name, 'GitSite');
     assert.equal(obj.site.version, 1);
     assert.equal(obj.site.author['full-name'], 'Crypto Michael');
+    assert.equal(obj.site.author.fullName, 'Crypto Michael');
+    assert.equal(obj.site['last-updated-at'].date, '2023-11-07');
+    assert.equal(obj.site.lastUpdatedAt.date, '2023-11-07');
+});
+
+test('generate book index', async (t) => {
+    await generateBookIndex('.', 'gitsite-guide');
 });
