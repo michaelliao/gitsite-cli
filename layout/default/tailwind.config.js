f3da5c9635fedbc796287c69814f3e8aaab34bc3
Michael Liao <askxuefeng@gmail.com>
2023-11-17 08:11:52 +0800

    update view

---
+++ layout/default/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: ['./static/custom-tailwind.txt'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
