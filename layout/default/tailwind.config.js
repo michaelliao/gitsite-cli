6b0940ba5d3e8018a767e06442acc110a309e949
Michael Liao <askxuefeng@gmail.com>
2023-11-22 23:14:44 +0800

    remove unused layout

--- layout/default/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: 'class',
-    content: ['./custom-tailwind.txt', './**/*.html'],
-    theme: {
-        extend: {},
-    },
-    plugins: ['forms', 'typography', 'aspect-ratio'],
-}
